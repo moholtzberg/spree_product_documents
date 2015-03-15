d9d41a16fc707d453c4908d2fe0a97e3d1275067
Moshe Holtzberg <moholtzberg@gmail.com>
2015-03-15 00:57:32 -0400

    change link to document in public view

--- app/overrides/product_document_tab.rb
+++ app/overrides/product_document_tab.rb
@@ -1,9 +1,11 @@
 Deface::Override.new(:virtual_path => "spree/admin/shared/_product_tabs",
-                 :name => "admin_product_document_tab",
-                 :original => '10edf2223cf4fe1ebcd351beb56472f5b623ea6b',
-                 :insert_after => "ul[data-hook=admin_product_tabs] > li:first",
-                 :text => "
-                  <%= content_tag :li, :class => ('active' if current == 'Documents') do %>
-                    <%= link_to_with_icon 'file', I18n.t('spree.admin.tab.documents'), admin_product_documents_path(@product) %>
-                  <% end if can?(:admin, Spree::Document) %>
-                ")
+	:name => "admin_product_document_tab",
+	:original => '10edf2223cf4fe1ebcd351beb56472f5b623ea6b',
+	:insert_after => "ul[data-hook=admin_product_tabs] > li:first",
+	:text => "
+		<% @product.documents.each do |d| %>
+			<li>
+				<%= link_to "<i class='fa fa-file fa-fw'></i> #{d.attachment_file_name}".html_safe, d.attachment.url(:product) %>
+			</li>
+	<% end %>
+")
