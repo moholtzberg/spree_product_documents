e32ceb00fa03ae7fdd2e2057712a887c04710097
Moshe Holtzberg <moholtzberg@gmail.com>
2015-03-15 01:32:37 -0400

    change virtual path

--- app/overrides/product_document_tab.rb
+++ app/overrides/product_document_tab.rb
@@ -1,4 +1,4 @@
-Deface::Override.new(:virtual_path => "spree/admin/shared/_product_tabs",
+Deface::Override.new(:virtual_path => "spree/shared/show",
 	:name => "admin_product_document_tab",
 	:original => '10edf2223cf4fe1ebcd351beb56472f5b623ea6b',
 	:insert_after => "ul[data-hook=admin_product_tabs] > li:first",
