07115f405ef3d403826e444a6812c2142e3f5f4e
Moshe Holtzberg <moholtzberg@gmail.com>
2015-03-15 01:40:38 -0400

    changed selector

--- app/overrides/product_document_tab.rb
+++ app/overrides/product_document_tab.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/shared/show",
 	:name => "admin_product_document_tab",
 	:original => '10edf2223cf4fe1ebcd351beb56472f5b623ea6b',
-	:insert_after => "ul[data-hook=admin_product_tabs] > li:first",
+	:insert_after => "[data-hook=product_properties]",
 	:partial => "spree/shared/document_links")
